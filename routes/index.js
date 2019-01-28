a31a4d7cfb9095fad66b77e2fd7dafd63a069dad
Ben Remnant <ben.remnant@gmail.com>
2019-01-28 16:36:25 +1300

    Adjusted bio page styling structure. Added resume button to bio page. Added resume page.

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ router.get('/', function(req, res, next) {
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Ben Remnant Development' });
+  res.render('contact', { title: 'Contact' });
 });
 
 /* GET contact page. */
 router.get('/resume', function(req, res, next) {
-  res.render('resume', { title: 'Ben Remnant Development' });
+  res.render('resume', { title: 'Ben Remnant Resume' });
 });
 
 /* GET contact page. */
 router.get('/resumeprint', function(req, res, next) {
-  res.render('resumeprint', { title: 'Ben Remnant Development' });
+  res.render('resumeprint', { title: 'Ben Remnant Resume' });
 });
 
 module.exports = router;
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET portfolio page. */
 router.get('/', function(req, res, next) {
-  res.render('portfolio', { title: 'Ben Remnant Development' });
+  res.render('portfolio', { title: 'Portfolio' });
 });
 
 router.get('/berd', function(req, res, next) {
-  res.render('portfolio/berd', { title: 'Ben Remnant Development' });
+  res.render('portfolio/berd', { title: 'Portfolio' });
 });
 
 router.get('/dinz', function(req, res, next) {
-  res.render('portfolio/dinz', { title: 'Ben Remnant Development' });
+  res.render('portfolio/dinz', { title: 'Portfolio' });
 });
 
 router.get('/zenergy', function(req, res, next) {
-  res.render('portfolio/zenergy', { title: 'Ben Remnant Development' });
+  res.render('portfolio/zenergy', { title: 'Portfolio' });
 });
 
 router.get('/plastic', function(req, res, next) {
-  res.render('portfolio/plastic', { title: 'Ben Remnant Development' });
+  res.render('portfolio/plastic', { title: 'Portfolio' });
 });
 
 router.get('/aah', function(req, res, next) {
-  res.render('portfolio/aah', { title: 'Ben Remnant Development' });
+  res.render('portfolio/aah', { title: 'Portfolio' });
 });
 
 module.exports = router;
