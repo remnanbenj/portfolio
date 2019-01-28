445d9ea93d96bd257e24f80553b0e6035a502de6
Ben Remnant <ben.remnant@gmail.com>
2019-01-28 17:37:45 +1300

    Contact page now sends me an email on use.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,27 @@ router.get('/', function(req, res, next) {
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+    res.render('contact', { title: 'Contact' });
+});
+
+router.post('/sendmessage', function(req, res, next) {
+    var name = req.query.name;
+    var email = req.query.email;
+    var message = req.query.message.split('<br>');
+    var msgString = "";
+    message.forEach(function(str) {
+        msgString += str + '\n';
+    });
+    var send = require('gmail-send')({
+        user: 'dinz.auto@gmail.com',
+        pass: 'hzzsawtuotzcgbyz',
+        to: 'ben.remnant@gmail.com',
+        subject: 'Contact on BERD website from ' + name,
+        text: 'From: ' + name + '\n'
+        + 'Email: ' + email + '\n\n'
+        + 'Message: ' + '\n' + msgString
+    })();
+    res.send('Message Sent');
 });
 
 /* GET contact page. */
